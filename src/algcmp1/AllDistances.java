a3f02ca8c535de243479f6c907d2bc37ebf60b51
Victor <kushnir.victor@gmail.com>
2021-05-14 07:25:50 +0300

    .

--- src/algcmp1/AllDistances.java
+++ src/algcmp1/AllDistances.java
@@ -1,12 +1,27 @@
 package algcmp1;
 
-public interface AllDistances {
-    /*Returns the distances matrix .*/
-    public int[][] distance_matrix();
+public class AllDistances implements AllDistancesInterface {
+    private int[] vertices_weights;
+    private int[][] edges_weights;
 
-    /*Returns the distance between given vertices u and v.*/
-    public int distance(int u, int v);
+    public AllDistances(int[] vertices_weights, int[][] edges_weights) {
+        //*TODO.
+        this.vertices_weights = vertices_weights;
+        this.edges_weights = edges_weights;
+    }
 
-    /* Returns a string of names of vertices which are located on the shortest path from the vertex u to v, separeted with '-'. */
-    public String path(int u, int v);
+    @Override
+    public int[][] distance_matrix() {
+        return new int[0][];
+    }
+
+    @Override
+    public int distance(int u, int v) {
+        return 0;
+    }
+
+    @Override
+    public String path(int u, int v) {
+        return null;
+    }
 }
