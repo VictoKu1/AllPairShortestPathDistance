7c42b1de0f9bbd6abd895d7a6fa50adcc2d84f73
Victor <kushnir.victor@gmail.com>
2021-05-14 07:13:17 +0300

    .

---
+++ src/algcmp1/AllDistances.java
@@ -0,0 +1,12 @@
+package algcmp1;
+
+public interface AllDistances {
+    /*Returns the distances matrix .*/
+    public int[][] distance_matrix();
+
+    /*Returns the distance between given vertices u and v.*/
+    public int distance(int u, int v);
+
+    /* Returns a string of names of vertices which are located on the shortest path from the vertex u to v, separeted with '-'. */
+    public String path(int u, int v);
+}
